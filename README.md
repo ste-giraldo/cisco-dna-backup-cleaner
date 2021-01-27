bd8ca1d92763134740f37ee3fa3290aaeab4ca7b
Stefano <stefano@nerv.it>
2021-01-27 14:24:57 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# cisco-dna-backup-cleaner
-Cisco DNA Center backup cleaner
+# cisco_dna_backup_cleaner.sh
+Cisco DNA Center is able to create scheduled backups of the config, devices' software images and so on, but there's not way to remove old backups, hence deleting should be done manually from DNA-C. Pretty annoying...
+I wrote this Bash script to automatically removing old backups basing on a retention period. 
+
+This script runs on GNU and require "curl" and "jq" packets installed, to working properly. Is intended to run from the path ~/script, but of course can be changed.
+
+Edit the script to change the URLs pointing to the hostname of your DNA-C installation. Please use cluster URL in case of 3 node deployment.
+Change also the base64 autorization key (currently ABC123= as placeholder), matching with your username and password used on DNA-C
+Change also the RETENTION value in order to match your needs.
+
+In my case, I scheduled the backups from DNA-C at Friday 9pm and the script runs from crontab at 11.30pm:
+
+30 23 * * 5 /bin/bash /PATH_TO/scripts/cisco_dna_backup_cleaner.sh >/dev/null 2>&1
---
+++ cisco_dna_backup_cleaner.sh
@@ -0,0 +1,55 @@
+### BEGIN INIT INFO
+# Provides:      network
+# Required-Start:   $network $remote_fs $syslog
+# Required-Stop:
+# Default-Start:   S
+# Default-Stop:
+# Short-Description:   Delete Cisco DNA Center Scheduled Backups
+# Version: 2.0
+### END INIT INFO
+
+#!/bin/bash
+
+# Change the RETENTION value to modify the number of backups to keep
+RETENTION=5
+echo "DNA Backups retention="$RETENTION
+
+# Retrive the TokenID via BASE64 encoded username and password to operate against DNA Center via API
+TOKEN="$(curl -s --insecure --request POST --url https://your.dna-center.local/dna/system/api/v1/auth/token  --header 'Authorization: basic ABC123=' --header 'content-type: application/json' | sed -e "s/{\"Token\":\"//" | sed -e "s/\"}//")"
+
+# Repeating function to count the number of backups, retrive the BackupID list via JSON query and write to dna_bck_id.list file
+REPEAT () {
+  curl -s --insecure --request GET --url https://your.dna-center.local/api/system/v1/maglev/backup --header 'x-auth-token: '${TOKEN} --header 'content-type: application/json' | jq '.response[] | [.backup_id,.status,.description,.start_timestamp] | @csv' > ~/script/dna_bck_id.list
+
+  COUNTER="$(cat ~/script/dna_bck_id.list | wc -l)"
+  echo "Currently available Backups="$COUNTER
+}
+# End function
+
+# Give the value to COUNTER for the first time with the number of online backups on DNA Center
+REPEAT
+while [ $COUNTER -gt $RETENTION ];
+ do
+ REPEAT
+
+# Retriving BackupID to delete
+  DEL_ID="$(cat ~/script/dna_bck_id.list | sort --field-separator=',' --key=4 | head -n 1 | awk -F "," {'print$1'} | cut -c 4- | sed 's/\\"//g';)"
+
+  echo Deleting BackupID=$DEL_ID
+
+# Send to DNA Center the request to delete the oldest backup
+  curl --insecure --request DELETE --url https://your.dna-center.local/api/system/v1/maglev/backup/$DEL_ID --header 'x-auth-token: '${TOKEN} --header 'content-type: application/json' ; echo ; echo
+  sleep 15s
+
+# Revaluing COUNTER after delete
+  curl -s --insecure --request GET --url https://your.dna-center.local/api/system/v1/maglev/backup --header 'x-auth-token: '${TOKEN} --header 'content-type: application/json' | jq '.response[] | [.backup_id,.status,.description,.start_timestamp] | @csv' > ~/script/dna_bck_id.list
+  COUNTER="$(cat ~/script/dna_bck_id.list | wc -l)"
+done
+
+COUNTER="$(cat ~/script/dna_bck_id.list | wc -l)"
+echo ""
+echo "Final Backups counter="$COUNTER
+
+#echo ""
+#echo "No DNA Backups candidate for deleting"
+#echo ""
